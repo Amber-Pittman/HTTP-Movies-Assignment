26b9097a476b8fa646184cec6116f296f86f61e0
Amber-Pittman <amber@amberpittman.com>
2020-03-19 19:52:47 -0400

    Building out the UpdateMovie file

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,19 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+
+const initialItem = {
+    id: "",
+    title: "",
+    director: "",
+    metascore: "",
+    stars: [],
+}
 
 export default function UpdateMovie(props) {
-    const [movie, setMovie] = useState({
-        id: "",
-        title: "",
-        director: "",
-        metascore: "",
-        stars: [],
-    })
+    const [movie, setMovie] = useState(initialItem);
+
+    useEffect(() => {
+        const itemToUpdate = props.items.find(item => {
+            return `${item.id}` === props.match.params.id;
+        });
+
+        console.log("itemToUpdate", itemToUpdate);
+        setMovie(itemToUpdate);
+    }, [props.items, props.match.params.id]);
 
     const handleChange = (event) => {
+        event.persist();
+        let value = event.target.value;
+        if (event.target.name === "stars") {
+            value = parseInt(value, 10);
+        }
+
         setMovie({
             ...movie
-            [event.target.name]: event.target.value
+            [event.target.name]: value
         })
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
     }
 
     return (
@@ -26,6 +49,31 @@ export default function UpdateMovie(props) {
                     placeholder="New Title"
                     value={movie.title}
                     onChange={handleChange} />
+                
+                <input
+                    type="text"
+                    name="director"
+                    placeholder="New Director"
+                    value={movie.director}
+                    onChange={handleChange} />
+
+                <input
+                    type="text"
+                    name="metascore"
+                    placeholder="New Metascore"
+                    value={movie.metascore}
+                    onChange={handleChange} />
+                
+                <input
+                    type="text"
+                    name="stars"
+                    placeholder="New Stars"
+                    value={movie.stars}
+                    onChange={handleChange} />
+
+                <button>
+                    Update
+                </button>
             </form>
         </div>
     )
